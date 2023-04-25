4ddd92eee221bdf26fc7552c6893a695c52aaa55
hanos <zhuhanos@gmail.com>
2023-04-25 13:04:46 +0800

    init

---
+++ FlyplayerView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using FlyleafLib.MediaPlayer;
+using FlyleafLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PlayerDemo
+{
+    /// <summary>
+    /// FlyplayerView.xaml 的交互逻辑
+    /// </summary>
+    public partial class FlyplayerView : Window
+    {
+        public FlyplayerView()
+        {
+            InitializeComponent();
+
+        }
+
+    }
+}
